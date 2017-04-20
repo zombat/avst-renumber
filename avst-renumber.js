0c88f92a8e22e71f2448f4177974674ffb618c88
Raymond Rizzo <raymond.rizzo@gmail.com>
2017-04-20 10:49:33 -0400

    Added index file renumbering and changed to csv bulk processing

--- avst-renumber.js
+++ avst-renumber.js
@@ -1,34 +1,104 @@
-// AVST Mailbox Renumber
+// AVST Mailbox Renumber v0.5.0
 // GPLv3
 // Raymond Rizzo
 
-var currentBox = process.argv[2];
-var endBox = process.argv[3];
+var renumberArray = [];
 
 var fs = require('fs');
+var numberOfProcesses = 0;
 
-while(currentBox <= endBox){
-	fs.readFile('./MB' + currentBox + '.xml', 'utf8', (err, file) => {
+// load CSV file if selected.
+if(process.argv[2] === '--csv'){
+	fs.readFile(process.argv[3], 'utf8', (err, file) => {
 		if(err){
-			// Show error for missing files.
-			console.log('NOT FOUND: ./MB' + currentBox + '.xml');
-		} else {
-			// Find 6xxxx extension to renumber mailbox to. 
-				var fileBuffer = file.toString('utf8');
-				var newBoxNumber = fileBuffer.match(/<DevAddr>(6\d{4})<\/DevAddr>/)[1];
-
-				// Make replacements.
-				var fileBuffer = fileBuffer.toString('utf8').replace(/<MBID>1\d{4}<\/MBID>/g, '<MBID>' + newBoxNumber + '<\/MBID>');
-				var fileBuffer = fileBuffer.replace(/<ID>1\d{4}<\/ID>/g, '<MBID>' + newBoxNumber + '<\/MBID>');
-				
-				// Write file with new name.
-				fs.writeFile('./new/MB' + newBoxNumber + '.xml', fileBuffer, function(err) {
-					if(err) {
-						return console.log(err);
-					}
-					console.log('/new/MB'+newBoxNumber+'.xml written');
-				}); 
+			console.log(err);
+		}
+		else{
+			var counter = 0;
+			renumberArray = splitCsv(file);
+			while(counter < renumberArray.length){
+				changeBoxes(renumberArray[counter][0],renumberArray[counter][1]);
+				counter++;
+			}
 		}
 	});
-	currentBox++;
+} else if(process.argv[2] === '--help'){
+	console.log('\n\n\nCSV file input:\n\tTo bulk renuber boxes based off of a .CSV file, run: avst-renumber.js --csv {filename.csv}\n\n\n');
+} else {
+	console.log('\n\n\nInvalid Input\nRun "avst-renumber.js --help" for more information.\n\n\n');
+}
+
+
+// Split file into something usable
+function splitCsv(fileIn){
+		fileIn = fileIn.replace(/\r/g, '');
+		var csvArray = fileIn.split('\n');
+		if(csvArray[csvArray.length-1] === ''){
+			csvArray.splice(-1);
+		}
+		var counter = 0;
+		while(counter < csvArray.length){
+			csvArray[counter] = csvArray[counter].split(',');
+			counter++;
+		}
+		return csvArray;
+}
+
+// Change MBID and ID in file.
+function changeBoxes(fromBox, toBox){
+		fs.readFile('./MB' + fromBox + '.XML', 'utf8', (err, file) => {
+			if(err){
+				// Show error for missing files.
+				console.log('ERROR READING: ' + fromBox);
+			} else {
+				// Find 6xxxx extension to renumber mailbox to. 
+					var fileBuffer = file.toString('utf8');
+					var findOldMBID = new RegExp('<MBID>' + fromBox + '<\/MBID>','gi');
+					var findOldID = new RegExp('<ID>' + fromBox + '<\/ID>','gi');
+					// Only process if this has the fromBox MBID.
+					if(fileBuffer.match(findOldMBID)){
+						numberOfProcesses++;
+						// Make replacements.
+						var fileBuffer = fileBuffer.replace(findOldMBID, '<MBID>' + toBox + '<\/MBID>');
+						var fileBuffer = fileBuffer.replace(findOldID, '<ID>' + toBox + '<\/ID>');
+						// Write file with new name.
+						fs.writeFile('./new/MB' + toBox + '.xml', fileBuffer, function(err) {
+							if(err) {
+								return console.log('FAILED TO WRITE: ./new/MB' + toBox + '.xml');
+							}
+							console.log('/new/MB'+toBox+'.xml written');
+							numberOfProcesses--;
+							if(numberOfProcesses === 0){
+								changeList();
+							}
+						});
+					} else {
+
+					}					
+			}
+		});
+}
+
+function changeList(){
+	fs.readFile('./MBLIST.xml', 'utf8', (err, file) => {
+		if(err){
+			// Show error for missing files.
+		} else {
+			var counter = 0;
+			var mailboxList = file.toString('utf8');
+			while(counter < renumberArray.length){
+				var oldId = new RegExp('<MBID>' + renumberArray[counter][0] + '<\/MBID>','gi');
+				var oldFileName = new RegExp('<MBFileName>MB' + renumberArray[counter][0] + '.XML<\/MBFileName>','gi');
+				mailboxList = mailboxList.replace(oldId, '<MBID>' + renumberArray[counter][1] + '</MBID>');
+				mailboxList = mailboxList.replace(oldFileName, '<MBFileName>MB' + renumberArray[counter][1] + '.XML</MBFileName>');
+				counter++;
+			}
+			fs.writeFile('./new/MBLIST.xml', mailboxList, function(err) {
+				if(err) {
+					return console.log('FAILED TO WRITE: ./new/MBLIST.xml');
+				}
+				console.log('/new/MBLIST.xml written');
+			});
+		}
+	});
 }
